dd978de48ca151f5feddf89df0f1e86b00e19fe9
poncich <amigohalooooo@gmail.com>
2026-01-27 16:54:52 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1qQzC1fL...", // ← 39+ символов
+  apiKey: "AIzaSyD1qQZclFtl-zARTf4w3qckygmXvo8Ld9w",
   authDomain: "soundcollab-fes9d.firebaseapp.com",
   projectId: "soundcollab-fes9d",
   storageBucket: "soundcollab-fes9d.appspot.com",
@@ -13,5 +7,5 @@ const firebaseConfig = {
   appId: "1:892512826888:web:abcdef1234567890abcdef"
 };
 
-// Инициализация Firebase 8
+// Инициализация Firebase 8 (БЕЗ import!)
 firebase.initializeApp(firebaseConfig);
