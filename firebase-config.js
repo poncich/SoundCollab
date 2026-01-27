25002b14bb468b447ea12d4c66897fee038187d4
poncich <amigohalooooo@gmail.com>
2026-01-27 16:27:22 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD1qQZclFtl-zARTf4w3qckygmXvo8Ld9w",
   authDomain: "soundcollab-fe59d.firebaseapp.com",
@@ -6,3 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "892512826880",
   appId: "1:892512826880:web:a346ee418fe79aab2f4ae5"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
