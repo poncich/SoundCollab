60b1aff6fc4bfbe0c9fe2c091f8a793bf36435ee
poncich <amigohalooooo@gmail.com>
2026-01-29 13:46:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,25 @@
 // firebase-config.js
-const backupConfig = {
-    apiKey: "AIzaSyC4RgPZ16xJE4TYTHhy5JgTmyWc4E1234",
-    authDomain: "soundcollab-test-12345.firebaseapp.com",
-    projectId: "soundcollab-test-12345",
-    storageBucket: "soundcollab-test-12345.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef1234567890abcdef"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKrjtyk9pXAdTRLI_Jm7pM-bRjvX7O3cI",
+  authDomain: "soundcollab-production.firebaseapp.com",
+  projectId: "soundcollab-production",
+  storageBucket: "soundcollab-production.firebasestorage.app",
+  messagingSenderId: "1024413284863",
+  appId: "1:1024413284863:web:1e051df31f3fd0b3f0cfca",
+  measurementId: "G-TYM8HQZ0ZS"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Выбираем конфигурацию
 const configToUse = firebaseConfig;
